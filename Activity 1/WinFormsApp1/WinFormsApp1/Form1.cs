95429fc74d658eb5e1e47e6e11fe0f9e01142705
jmtz258 <109483475+jmtz258@users.noreply.github.com>
2022-07-17 15:10:29 -0600

    Add files via upload

---
+++ Activity 1/WinFormsApp1/WinFormsApp1/Form1.cs
@@ -0,0 +1,24 @@
+using System;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnClickThis_Click(object sender, EventArgs e)
+        {
+            lblHelloWorld.Text = "Hello World!";
+        }
+    }
+}
